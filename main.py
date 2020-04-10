c0e65c66adb46ae47996500b36bdf31298677af2
Psyrkoz <psyrkoz68@gmail.com>
2020-04-10 12:00:41 +0200

    Ajout du logging dans le fichier main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from connexion import GoogleConnexion
+import logging
 
 
 if(__name__ == "__main__"):
+    logging.basicConfig(filename = "log", filemode = 'a', format= "%(asctime)s - %(levelname)s - %(message)s", level = logging.DEBUG)
+    logging.info("Demarrage de l'application")
     gc = GoogleConnexion()
